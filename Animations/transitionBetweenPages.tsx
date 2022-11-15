73e004a0d4aecaca0825f1b6afc726c880c6e619
AndreyNovaes <andreynovaespro@gmail.com>
2022-11-15 08:02:18 -0300

    feat: add component of page transitions animation

---
+++ Animations/transitionBetweenPages.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+
+export default function TransitionBetweenPages({ children }: React.PropsWithChildren<Record<string, unknown>>) {
+  const router = useRouter()
+
+  const variants = {
+    initialState: {
+      opacity: 0,
+      clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+    },
+    animateState: {
+      opacity: 1,
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+    },
+    exitState: {
+      clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
+    },
+  }
+
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 1.5,
+        }}
+        variants={variants}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,15 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useRouter } from 'next/router'
+import TransitionBetweenPages from 'Animations/transitionBetweenPages'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  const variants = {
-    initialState: {
-      opacity: 0,
-      clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-    },
-    animateState: {
-      opacity: 1,
-      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-    },
-    exitState: {
-      clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)',
-    },
-  }
-
 
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        key={router.route}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        transition={{
-          duration: 1.5,
-        }}
-        variants={variants}
-      >
+    <TransitionBetweenPages>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </motion.div>
-    </AnimatePresence>
+    </TransitionBetweenPages>
   )
 }
