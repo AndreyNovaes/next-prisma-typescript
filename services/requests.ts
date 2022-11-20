0ee9d3c9d4ec4538c04ec9ac9096da6d9f8789aa
AndreyNovaes <andreynovaespro@gmail.com>
2022-11-20 19:44:07 -0300

    typo: fix url without api p-fix

--- services/requests.ts
+++ services/requests.ts
@@ -1,6 +1,6 @@
 import { form } from '@/components/Main-Content/Contact';
 
-const baseURL = 'http://andrey-novaes.me';
+const baseURL = 'http://andrey-novaes.me/api';
 
 export async function sendMail({ name, email, message }: form) {
   const response = await fetch(`${baseURL}/mail`, {
