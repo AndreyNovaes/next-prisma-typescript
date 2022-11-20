e04df5b77e186befa977737ec0d31cc431ae7d6c
AndreyNovaes <andreynovaespro@gmail.com>
2022-11-20 11:02:23 -0300

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import TransitionBetweenPages from 'Animations/transitionBetweenPages'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <TransitionBetweenPages>
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
     </TransitionBetweenPages>
   )
