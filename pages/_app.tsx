14dc19120650f0d90200451a908d3cda54e71920
AndreyNovaes <andreynovaespro@gmail.com>
2022-11-22 06:53:42 -0300

    feat: add some seo improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,22 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import TransitionBetweenPages from 'Animations/transitionBetweenPages'
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <TransitionBetweenPages>
+      <Head>
+        <title>Andrey Novaes</title>
+        <meta name="description" content="Portfólio do desenvolvedor Andrey Novaes" />
+        <meta property="og:title" content="Andrey Novaes - Portfólio" />
+        <meta property="og:description" content="Andrey Novaes - Portfólio" />
+        <meta property="og:image" content="public/images/about.jpeg" />
+        <meta property="og:url" content="https://andrey-novaes.me/" />
+        <meta property="og:site_name" content="Andrey Novaes - Portfólio" />
+        <meta property="og:type" content="website" />
+      </Head>
         <Layout>
           <Component {...pageProps} />
           <Analytics />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,6 @@ export default function Document() {
     <Html lang='pt-BR'>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Portfólio do desenvolvedor Andrey Novaes" />
-        <meta property="og:title" content="Andrey Novaes - Portfólio" />
-        <meta property="og:description" content="A busca de um portfólio com a minha cara" />
-        <meta property="og:image" content="public/images/about.jpeg" />
-        <meta property="og:url" content="https://andrey-novaes.me/" />
-        <meta property="og:site_name" content="Andrey Novaes - Portfólio" />
-        <meta property="og:type" content="website" />
-
       </Head>
       <body>
         <Main />
